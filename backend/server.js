3d0c7530dc2d1e9430d829e41fad72a086ac315b
VexIvannnnnnn <sumangvex@gmail.com>
2025-08-13 22:10:51 +0800

    Deletion: Removed node folder outside backend for cleaner codebase

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectMongo from './config/mongo.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
+connectMongo();
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT}`);
